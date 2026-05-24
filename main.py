c81459424b2aa7a08379daf4ce0b5b01db96b6ee
Saxam Rawat <saxamrawat@gmail.com>
2026-05-24 22:32:58 +0530

    Setup Alembic Migrations and Initialize database versioning

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Mount Static Files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(admin.router)
